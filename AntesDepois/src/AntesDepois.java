8e8302d0014f516fd6f63f383ba91ce0643a6336
mcosta.df <mcosta.df@gmail.com>
2018-07-31 12:39:54 -0300

    teste

---
+++ AntesDepois/src/AntesDepois.java
@@ -0,0 +1,50 @@
+
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class AntesDepois {
+
+	@BeforeAll
+	public static void anteTudo() {
+		System.out.println("@beforeClass");
+	}
+	
+	@AfterAll
+	public static void depoisDeTudo() {
+		System.out.println("@AfterClass");
+	}
+	
+	@BeforeEach
+	public void before() {
+		System.out.println("@Before");
+	}
+	
+	@AfterEach
+	public void after() {
+		System.out.println("@After");
+	}
+	
+	@Test
+	public void test1() {
+		System.out.println("Teste 1");
+	}
+	
+	@Test
+	public void test2() {
+		System.out.println("Teste 2");
+	}
+	
+	@Test
+	public void test3() {
+		System.out.println("Teste 3");
+	}
+
+}
